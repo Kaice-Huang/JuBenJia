caca2750dcbd1ec6b5847b87d7b9380e0f25486b
Kaice-Huang <kaicegs55555@gmail.com>
2025-02-08 17:00:47 +0000

    更新 ppa.py

--- backend/ppa.py
+++ backend/ppa.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return jsonify({"message": "欢迎来到剧本家游戏服务器"})
+    return jsonify({"message": "欢迎来到游戏服务器"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=False)
